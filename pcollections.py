d1a691dcb591d59dddbd2d573dbfd1c7e4ee8228
Bryant Phan <bphan4bryant@yahoo.com>
2022-02-14 22:42:28 -0800

    rest of methods implemented

--- pcollections.py
+++ pcollections.py
@@ -13,16 +13,16 @@ def pnamedtuple(type_name, field_names, mutable = False,  defaults =  {}):
     # put your code here
     # bind class_definition (used below) to the string constructed for the class
     
-    pattern = '[a-zA-Z][\w]*'
+    pattern = r'[a-zA-Z][\w]*'
     
     # type_name legality
-    if type_name in keyword.kwlist or not bool(re.match(pattern, type_name)):
+    if type(type_name) != str or type_name in keyword.kwlist or not bool(re.match(pattern, type_name)):
         raise SyntaxError(f'{type_name} is not a legal string or is a keyword')
     
     # field_names legality
     if type(field_names) == list: names = field_names
     elif type(field_names) == str: names = re.split("[, ]+", field_names)
-    else: raise TypeError
+    else: raise SyntaxError(f'{field_names} must be of type list or str')
     for name in names:
         if name in keyword.kwlist or not bool(re.match(pattern, name)):
             raise SyntaxError(f'{name} is not a legal string or is a keyword')
@@ -33,40 +33,105 @@ def pnamedtuple(type_name, field_names, mutable = False,  defaults =  {}):
             raise SyntaxError(f'{key} not a valid argument name')
     
     class_template = '''\
-    class {class_name}:
-        _fields = [{fields}]
-        _mutable = {mutable}
-        
-        {init_block}
-        
-        {repr_block}
-        
-        {get_block}
-        
-        {getitem_block}
-        
-        {eq_block}
-        
-        {_asdict_block}
-        
-        {_make_block}
-        
-        {_replace_block}
-        
-        #{setattr_block}
-    '''
+class {class_name}:
+    _fields = {fields}
+    _mutable = {mutable}
+    
+    {init_block}
+    
+    {repr_block}
+    
+    {get_block}
+    
+    {getitem_block}
+    
+    {eq_block}
+    
+    {_asdict_block}
+    
+    {_make_block}
+    
+    {_replace_block}
+'''
 
     def gen_init() -> str:
-        my_string = 'def __init__(self, {params}):\n'.format(params = ', '.join([name if name not in defaults else f'{name}={defaults[name]}' for name in field_names]))
+        my_string = 'def __init__(self, {params}):\n'.format(params = ', '.join([name if name not in defaults else f'{name}={defaults[name]}' for name in names]))
         for name in names:
-            my_string += f'    self.{name} = {name}\n' 
+            my_string += '''        self.{name} = {name}\n'''.format(name=name)
         return my_string
         
+    def gen_repr():   
+        field_args = []
+        for field_name in names:
+            field_args.append(f'{field_name}=self.{field_name}')
+        
+        return '''
+    def __repr__(self):
+        return '{class_name}({field_args})' '''.format(class_name=type_name, field_args=','.join(field_args))
+    
+    def gen_get():
+        get_funcs = ''
+        for field_name in names:
+            get_funcs += '''
+    def get_{name}(self):
+        return self.{name}
+        '''.format(name=field_name)
+        return get_funcs
+    
+    def gen_getitem():
+        return '''
+    def __getitem__(self, idx):
+        if type(idx) == int and idx in range(len(self._fields)):
+            return eval(f'self.get_{self._fields[idx]}')
+        elif type(idx) == str and idx in self._fields:
+            return eval(f'self.get_{idx}')
+        else:
+            raise IndexError'''
+    
+    def gen_eq():
+        return '''
+    def __eq__(self, comparison):
+        if type(comparison) == type(self):
+            for idx in range(len(self._fields)):
+                if self.__getitem__(idx) != comparison.__getitem__(idx):
+                    return False
+            return True
+        else:
+            return False'''
+    
+    def gen_asdict():
+        add_keys = ''
+        for field_name in names:
+            add_keys += '''
+        attr_dict[{field_name}] = self.get_{field_name}()'''.format(field_name = field_name)
+        return '''
+    def _asdict():
+        attr_dict = {{}}
+        {add_keys}'''.format(add_keys=add_keys)
+        
     def gen_make():
-        return '@staticmethod\ndef _make(iterable):\nreturn {type_name'
+        return '''
+    @staticmethod
+    def _make(iterable):
+        return {type_name}(*iterable)'''.format(type_name = type_name)
+    
+    def gen_replace():
+        return '''
+    def _replace(**kargs):
+        for karg in kargs:
+            if karg not in {names}:
+                raise TypeError
+        if self._mutable:
+            for karg in kargs:
+                exec(f'self.{{karg}} = {{kargs[karg]}}')
+            return None
+        else:
+            return {type_name}(**kargs)'''.format(names = names, type_name = type_name)
+                
+    class_definition = class_template.format(class_name = type_name, fields = names, mutable = mutable, init_block = gen_init(), repr_block = gen_repr(), get_block = gen_get(), getitem_block = gen_getitem(), eq_block = gen_eq(), _asdict_block = gen_asdict(), _make_block = gen_make(), _replace_block = gen_replace())
 
     # Debugging aid: uncomment next call to show_listing to display source code
-    # show_listing(class_definition)
+    #show_listing(class_definition)
     
     # Execute str in class_definition in name_space; then bind the attribute
     #   source_code to the class_definition str; finally, return the class
